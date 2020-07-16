1280f5b3d1eac7fe01c71bd7e38e2a880c3ec062
Youhei Oonuma <oonuma617@gmail.com>
2020-07-16 22:08:10 +0900

    add appbar

--- lib/widget/list.dart
+++ lib/widget/list.dart
@@ -14,6 +14,9 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Listview"),
+      ),
       body: ListView(
         padding: const EdgeInsets.all(8),
         children: <Widget>[
